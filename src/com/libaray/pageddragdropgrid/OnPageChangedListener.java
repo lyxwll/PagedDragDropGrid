a9d1d2a60b09ad57cf8c83c90a721fdbe1bfe59e
wanglili <jyu4228716@163.com>
2015-08-05 16:35:04 +0800

    分页拖放网格布局Library

---
+++ src/com/libaray/pageddragdropgrid/OnPageChangedListener.java
@@ -0,0 +1,14 @@
+package com.libaray.pageddragdropgrid;
+
+public interface OnPageChangedListener {
+
+	/**
+	 * called when the grid is scrolled to another page
+	 * 
+	 * @param sender
+	 *            grid
+	 * @param newPageNumber
+	 *            0 based
+	 */
+	public void onPageChanged(PagedDragDropGrid sender, int newPageNumber);
+}
